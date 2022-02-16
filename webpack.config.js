199c15e38682d580daac2a47fa7e35f7a6400adb
Mia Kang <miakangnyc@gmail.com>
2022-02-15 21:37:56 -0500

    nevermind took package-lock out of gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,42 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
-const path = require('path')
+const path = require('path');
 
 module.exports = {
-    entry: './client/index.js',
-    output: {
-        path: path.join(__dirname, 'build'),
-        filename: 'bundle.js',
-    },
-    plugins: [new HtmlWebpackPlugin({
-        template: path.join(__dirname, './client/index.html')
-        }),
-        new ESLintPlugin(options)
-    ],
-    mode: process.env.NODE_ENV,
-    module: {
-        rules: [
-            {
-                test: /\.jsx?/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react']
-                    }
-                }
-            },
-        ]
-    },
-    devServer: {
-        static: {
-            publicPath: '/build',
-            directory: path.join(__dirname, 'build'),
+  entry: './client/index.js',
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, './client/index.html'),
+    }),
+    new ESLintPlugin(options),
+  ],
+  mode: process.env.NODE_ENV,
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
         },
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      publicPath: '/build',
+      directory: path.join(__dirname, 'build'),
     },
-    resolve: {
-        // Enable importing JS / JSX files without specifying their extension
-        extensions: ['.js', '.jsx'],
-    },
-}
+  },
+  resolve: {
+    // Enable importing JS / JSX files without specifying their extension
+    extensions: ['.js', '.jsx'],
+  },
+};
