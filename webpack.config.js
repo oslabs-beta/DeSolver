4eb361c5b1199ce4fca96ebfba9130e9ca9c076c
Julia Hickey <hijulia1136@Julias-MacBook-Pro.local>
2022-02-16 20:21:14 -0800

    added ts compiler via webpack and tsconfig

--- webpack.config.js
+++ webpack.config.js
@@ -4,39 +4,49 @@ const path = require('path');
 const { ESLint } = require('eslint');
 
 module.exports = {
-    entry: './client/index.js',
-    output: {
-        path: path.join(__dirname, 'build'),
-        filename: 'bundle.js',
-    },
-    plugins: [new HtmlWebpackPlugin({
-        template: path.join(__dirname, './client/index.html')
-        }),
-        new ESLintPlugin()
-    ],
-    mode: process.env.NODE_ENV,
-    module: {
-        rules: [
-            {
-                test: /\.jsx?/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react']
-                    }
-                }
-            },
-        ]
-    },
-    devServer: {
-        static: {
-            publicPath: '/build',
-            directory: path.join(__dirname, 'build'),
+  entry: './client/index.js',
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, './client/index.html'),
+    }),
+    new ESLintPlugin(),
+  ],
+  mode: process.env.NODE_ENV,
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-typescript',
+            ],
+          },
         },
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      publicPath: '/build',
+      directory: path.join(__dirname, 'build'),
     },
-    resolve: {
-        // Enable importing JS / JSX files without specifying their extension
-        extensions: ['.js', '.jsx'],
-    },
-}
+  },
+  resolve: {
+    // Enable importing JS / JSX files without specifying their extension
+    extensions: ['.js', '.jsx', '.tsx', '.ts'],
+  },
+};
