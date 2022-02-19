76c80e6e3335b3eb2697d4d6afe53c7f27e3369c
Mia Kang <miakangnyc@gmail.com>
2022-02-19 17:17:56 -0500

    added Population query but cannot get population from ext API

--- server/server.js
+++ server/server.js
@@ -1,19 +1,33 @@
+require('dotenv').config();
 const PORT = 3000;
 const { ApolloServer, gql } = require('apollo-server-express');
 const express = require('express');
 const app = express();
 const axios = require('axios');
+const db = require('../models/elephantConnect');
 
 const typeDefs = gql`
   type Query {
     hello: String
     getPopByCountry(country: String): Int
+    getAllCountries: [Country]
+    population: Population
+  }
+  type Population {
+    population: Int
+  }
+  type Country {
+    country_id: String
+    country_name: String
+    region_id: Int
+    population: Population
   }
 `;
 
 const resolvers = {
   Query: {
     hello: () => 'Hello world!',
+
     getPopByCountry: async (_, { country }, context, info) => {
       try {
         const queryRes = await axios({
@@ -29,6 +43,47 @@ const resolvers = {
         console.log(queryRes.data);
         const population = queryRes.data.body.population;
         return population;
+      } catch (err) {
+        console.log('error with getPopByCountry: ', err);
+      }
+    },
+
+    getAllCountries: async (_, __, context, info) => {
+      try {
+        const query = `
+        SELECT * FROM countries;
+        `;
+        const allCountries = await db.query(query);
+        console.log(allCountries.rows);
+        return allCountries.rows;
+        // return allCountries.rows.map((country) => {
+        //   return country.country_name;
+        // });
+      } catch (err) {
+        console.log('error in getAllCountries: ', err);
+      }
+    },
+  },
+
+  Population: {
+    population: async (parent, __, context, info) => {
+      console.log('in the population query');
+      try {
+        let name = parent.country_name;
+        console.log('NAME in Population Query: ', name);
+        const queryRes = await axios({
+          method: 'GET',
+          url: 'https://world-population.p.rapidapi.com/population',
+          params: { country_name: name },
+          headers: {
+            'x-rapidapi-host': 'world-population.p.rapidapi.com',
+            'x-rapidapi-key':
+              '7f41d5564dmsh59dfbf8c3bf6336p160c99jsn22c3b8738e61',
+          },
+        });
+        console.log(queryRes.data);
+        const population = queryRes.data.body.population;
+        return { population };
       } catch (err) {
         console.log('error with getPopByCountry: ', err);
       }
