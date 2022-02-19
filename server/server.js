371ac76a623fab7262b73f48b03cce3797d2eace
Mia Kang <miakangnyc@gmail.com>
2022-02-19 15:08:46 -0500

    successful resolver connection to ext API

--- server/server.js
+++ server/server.js
@@ -2,16 +2,37 @@ const PORT = 3000;
 const { ApolloServer, gql } = require('apollo-server-express');
 const express = require('express');
 const app = express();
+const axios = require('axios');
 
 const typeDefs = gql`
   type Query {
     hello: String
+    getPopByCountry(country: String): Int
   }
 `;
 
 const resolvers = {
   Query: {
     hello: () => 'Hello world!',
+    getPopByCountry: async (_, { country }, context, info) => {
+      try {
+        const queryRes = await axios({
+          method: 'GET',
+          url: 'https://world-population.p.rapidapi.com/population',
+          params: { country_name: country },
+          headers: {
+            'x-rapidapi-host': 'world-population.p.rapidapi.com',
+            'x-rapidapi-key':
+              '7f41d5564dmsh59dfbf8c3bf6336p160c99jsn22c3b8738e61',
+          },
+        });
+        console.log(queryRes.data);
+        const population = queryRes.data.body.population;
+        return population;
+      } catch (err) {
+        console.log('error with getPopByCountry: ', err);
+      }
+    },
   },
 };
 
