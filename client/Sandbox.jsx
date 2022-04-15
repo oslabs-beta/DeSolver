272794a4ffe5d04d196aa23baebce8b05ace5016
AlexGurfinkel <gurfinkel.alexander@gmail.com>
2022-04-14 20:58:49 -0700

    playground install

--- client/Sandbox.jsx
+++ client/Sandbox.jsx
@@ -5,7 +5,9 @@ const Sandbox = () => {
     <div>
         <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
           <div className="px-4 py-6 sm:px-0">
-            <div className="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
+            <div className="h-96">
+              <iframe className="h-full w-full" src="https://nhzvc.csb.app/"></iframe>
+            </div>
           </div>
         </div>
     </div>
