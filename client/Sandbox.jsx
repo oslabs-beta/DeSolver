5f21f8f8296f6c42b5ec1acf64650ac47f2e8728
Julia Hickey <hijulia1136@Julias-MacBook-Pro.local>
2022-04-26 17:56:33 -0700

    updated

--- client/Sandbox.jsx
+++ client/Sandbox.jsx
@@ -3,15 +3,18 @@ import React from 'react';
 const Sandbox = () => {
   return (
     <div>
-        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-          <div className="px-4 py-6 sm:px-0">
-            <div className="h-96">
-              <iframe className="h-full w-full" src="https://nhzvc.csb.app/"></iframe>
-            </div>
+      <div className='max-w-7xl mx-auto py-6 sm:px-6 lg:px-8'>
+        <div className='px-4 py-6 sm:px-0'>
+          <div className='h-96'>
+            <iframe
+              className='h-full w-full'
+              src='https://nhzvc.csb.app/'
+            ></iframe>
           </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Sandbox;
