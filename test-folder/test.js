c9b69b5e9343339dd7365167ee1601c4443e97be
Mia Kang <miakangnyc@gmail.com>
2022-05-04 21:11:40 -0400

    getting SyntaxError: Unexpected identifier

---
+++ test-folder/test.js
@@ -0,0 +1,11 @@
+// import { Desolver } from '../index.js/desolver';
+
+const { Desolver } = require('desolver');
+
+// let testconnect = new Desolver({
+//   cacheDesolver: true,
+//   applyResolverType: 'All',
+// });
+
+console.log('hasNext: ', Desolver.hasNext);
+// console.log('cacheDesolver: ', testconnect.cacheDesolver);
